6aede4c1ba67aa94c56fd6cb90417d32411d01d5
Gwendolyn Wahl <gwen.wahl@owenscorning.com>
2015-07-01 09:34:33 -0400

    Added HPBE Configuration, this branch will be for HPBE

--- vendor/assets/javascripts/redactor-rails/config.js
+++ vendor/assets/javascripts/redactor-rails/config.js
@@ -3,14 +3,20 @@ window.init_redactor = function(){
   var csrf_param = $('meta[name=csrf-param]').attr('content');
   var params;
   if (csrf_param !== undefined && csrf_token !== undefined) {
-    params = csrf_param + "=" + encodeURIComponent(csrf_token);
+      params = csrf_param + "=" + encodeURIComponent(csrf_token);
   }
-  $('.redactor').redactor(
-    { "imageUpload":"/redactor_rails/pictures?" + params,
-      "imageGetJson":"/redactor_rails/pictures",
-      "path":"/assets/redactor-rails",
-      "css":"style.css"}
-  );
+  $('.redactor').redactor({
+        // You can specify, which ones plugins you need.
+        // If you want to use plugins, you have add plugins to your
+        // application.js and application.css files and uncomment the line below:
+        "plugins": ['fullscreen', 'video'],
+        "imageUpload":"/redactor_rails/pictures?" + params,
+        "imageGetJson":"/redactor_rails/pictures",
+        "fileUpload":"/redactor_rails/documents?" + params,
+        "fileGetJson":"/redactor_rails/documents",
+        "path":"/assets/redactor-rails",
+        "css":"style.css"
+      });
 }
 
 $(document).on( 'ready page:load', window.init_redactor );
